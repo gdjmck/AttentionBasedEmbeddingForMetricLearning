371394fb1000a21012d77d5f9c6e7889a2ee9212
chenkang <lv23ck@hotmail.com>
2019-08-01 12:25:54 +0800

    fix bug

--- train.py
+++ train.py
@@ -72,7 +72,9 @@ if __name__ == '__main__':
                     imgs = MetricData.tensor2img(batch[0])
                     print('number of images in batch:', len(imgs), imgs[0].shape, imgs[0].min(), imgs[0].max())
                     print('number of attentions in batch:', len(atts), atts[0].shape)
-                    vis.images(np.concatenate([cv2.resize(np.repeat(atts[i].cpu().numpy()[0, ...].mean(axis=0)[...,np.newaxis], 3, axis=-1), (224, 224))[np.newaxis]*255 for i in range(3)]), \
+                    att_imgs = np.concatenate([cv2.resize(np.repeat(atts[i].cpu().numpy()[0, ...].mean(axis=0)[...,np.newaxis], 3, axis=-1), (224, 224))[np.newaxis]*255 for i in range(3)])
+                    print(att_imgs.shape)
+                    vis.images(att_imgs, \
                         win=i+1000, opts=dict(title='Att_%d'%i))
                     top_4[i] = {'fn': batch[1][0], 'query': query.cpu().numpy(), 'top_8': []}
                     vis.image(np.transpose(cv2.imread(os.path.join(args.img_folder_test, top_4[i]['fn']))[..., ::-1], (2, 0, 1)), \
