f1033eb68f9d1a798991eada7ee1a151ac7c1bad
chenkang <lv23ck@hotmail.com>
2019-08-01 16:00:26 +0800

    fix bug

--- model.py
+++ model.py
@@ -94,8 +94,8 @@ class MetricLearner(GoogLeNet.GoogLeNet):
         # N x 3 x 224 x 224
         sp = self.feat_spatial(x)
         att_input = self.att_prep(sp)
-        atts = [torch.sigmoid(self.att[i](att_input)) for i in len(self.att)]
-        embedding = torch.cat([self.feat_global(atts[i]*sp).unsqueeze(1) for i in len(atts)], 1)
+        atts = [torch.sigmoid(self.att[i](att_input)) for i in range(len(self.att))]
+        embedding = torch.cat([self.feat_global(atts[i]*sp).unsqueeze(1) for i in range(len(atts))], 1)
         return (embedding, atts) if ret_att else embedding
         '''
         embeddings = torch.cat([self.feat_global(att*sp) for att in atts], 1)
