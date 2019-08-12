d72fcfa12e6369d7abf02f2e57c9b4e2a3dbfd61
chenkang <lv23ck@hotmail.com>
2019-08-12 15:35:57 +0800

    save embedding to pickle file

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
+import torch
 import torchvision
+import pickle
 from PIL import Image
 import train
 import os
@@ -13,5 +15,19 @@ if __name__ == '__main__':
     model = train.model
 
     data = train.convert_dataset(os.path.join(args.img_folder_test, 'train'), loader_test)
-    print('class to idx:', data.class_to_idx)
-    print(len(data.targets), data.targets)
+    # print('class to idx:', data.class_to_idx)
+    # print(len(data.targets), data.targets)
+
+    dataset = torch.utils.data.DataLoader(data)
+    embeddings = {}
+    for i, img in enumerate(dataset):
+        label = data.targets[i]
+        embedding = model(img).cpu().numpy()
+        if label not in embeddings.keys():
+            embeddings[label] = [embedding]
+        else:
+            embeddings[label].append(embedding)
+    
+    with open('embeddings.pkl', 'wb') as f:
+        pickle.dump(embeddings, f)
+        print('saved embedding.')        
