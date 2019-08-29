7a8b6cabe73ada93c63796e732d8d19becbd2886
chenkang <lv23ck@hotmail.com>
2019-08-29 11:20:41 +0800

    add embedding visualization in tensorboard

---
+++ vis_embed.py
@@ -0,0 +1,30 @@
+import torch
+from tensorboardX import SummaryWriter
+import pickle
+import numpy as np
+from PIL import Image
+import torchvision.transforms as transforms
+import argparse
+
+def parse_args():
+	parser = argparse.ArgumentParser(description='Visualize embedding in tensorboard.')
+	parser.add_argument('--embeddings', type=str, required=True, help='embedding pickle file.')
+	parser.add_argument('--save-folder', type=str, default='./runs', help='directory to save tensorboard file.')
+	return parser.parse_args()
+
+if __name__ == '__main__':
+	args = parse_args()
+	assert args.embeddings.endswith('.pkl')
+	writer = SummaryWriter(args.save_folder)
+	with open(args.embeddings, 'rb') as f:
+		data = pickle.load(f)
+	labels = []
+	feats = []
+	for label in data.keys():
+		labels.extend([label]*len(data[label]))
+		feats.extend(data[label])
+
+	feats = [torch.Tensor(feat[0]) for feat in feats]
+	feats = torch.cat(feats, 0)
+	writer.add_embedding(feats, metadata=labels)
+	writer.close()
