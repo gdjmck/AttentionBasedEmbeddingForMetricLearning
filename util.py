be85d6284b2f0be72a6bafe44f23d9773e6c1379
root <lv23ck@hotmail.com>
2019-09-11 16:42:47 +0800

    reduce font size in tensorboard

--- util.py
+++ util.py
@@ -35,7 +35,7 @@ def plot_grad_flow_v2(named_parameters):
             if p.grad is None:
                 #print('no grad:', n)
                 continue
-            n = n.replace('.weight', '').replace('inception', '').replace('branch', 'B')
+            n = n.replace('.weight', '').replace('inception', '').replace('branch', 'B').replace('conv', 'c')
             layers.append(n)
             ave_grads.append(p.grad.abs().mean())
             max_grads.append(p.grad.abs().max())
@@ -44,7 +44,7 @@ def plot_grad_flow_v2(named_parameters):
     plt.bar(np.arange(len(max_grads)), max_grads, alpha=0.1, lw=1, color="c")
     plt.bar(np.arange(len(max_grads)), ave_grads, alpha=0.1, lw=1, color="b")
     plt.hlines(0, 0, len(ave_grads)+1, lw=2, color="k" )
-    plt.xticks(range(0,len(ave_grads), 1), layers, rotation="vertical")
+    plt.xticks(range(0,len(ave_grads), 1), layers, rotation="vertical", fontsize='xx-small')
     plt.xlim(left=0, right=len(ave_grads))
     plt.ylim(bottom = -0.001, top=0.02) # zoom in on the lower gradient regions
     plt.xlabel("Layers")
